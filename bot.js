b67d441992a611c27db64adecc1f1b8e54ddcfad
carlos-aguilar <carlos_aguilar_ch@yahoo.com>
2018-01-30 11:24:12 -0600

    initial commit

--- bot.js
+++ bot.js
@@ -30,20 +30,20 @@ bot.on('conversationUpdate', function (message) {
 bot.dialog('MasInfo', [
     function (session) {
         session.send("Gracias por permitirnos contactarle!.");
-        builder.Prompts.text(session, "Cuál es su nombre2");        
+        builder.Prompts.text(session, "Cuál es su nombre?");        
     },
     function (session, results ) {
         session.dialogData.nombre = results.response;
-        builder.Prompts.text(session, "Cuál es su correo:");       
+        builder.Prompts.text(session, "¿Cuál es su correo:?");       
         
     },
     function (session, results) {
         session.dialogData.email = results.response;
-        builder.Prompts.text (session, "Cuál es su teléfono de contacto?");        
+        builder.Prompts.text (session, "¿Cuál es su teléfono de contacto?");        
     },
     function (session, results) {
         session.dialogData.telefono = results.response;
-        builder.Prompts.text (session, "Qué tema le interesa?");        
+        builder.Prompts.text (session, "¿Qué tema le interesa?");        
     },
     function (session, results) {
         
@@ -65,43 +65,22 @@ bot.dialog('MasInfo', [
 
 bot.dialog('#Demo', [
     function (session) {
-        session.send("Entrando al modo demo, te mostrare algunas de nuestras capacidades");
-        //builder.Prompts.text(session, "Cuál es su nombre");  
-        var cards = getCardsAttachments();
+        session.send("Entrando al modo demo, te mostrare algunas de nuestros servicios");
+        builder.Prompts.text(session, "Cuál es su nombre?"); 
+        
+        
+    },
+    function (session,results) {
+            session.dialogData.nombre = results.response;
+            var cards = getCardsAttachments();
         
             // create reply with Carousel AttachmentLayout
             var reply = new builder.Message(session)
                 .attachmentLayout(builder.AttachmentLayout.carousel)
                 .attachments(cards);
             session.send(reply);
-        
-
-
-
-    },
-    function (session, results ) {
-        session.dialogData.nombre = results.response;
-        builder.Prompts.text(session, "Cuál es su correo:");       
-        
-    },
-    function (session, results) {
-        session.dialogData.email = results.response;
-        builder.Prompts.text (session, "Cuál es su teléfono de contacto?");        
-    },
-    function (session, results) {
-        session.dialogData.telefono = results.response;
-        builder.Prompts.text (session, "Qué tema le interesa?");        
-    },
-    function (session, results) {
-        
-        session.dialogData.tema = results.response;
-                // 
-        session.send(`Datos de Contacto Nombre:  ${session.dialogData.nombre}<br/> Email: ${session.dialogData.email} `);
-        session.send(`Tema:  ${session.dialogData.tema} `);
-        mensaje.correoenviado(session.dialogData.nombre, session.dialogData.email,session.dialogData.telefono,session.dialogData.tema);
-        session.send(`fin de contacto   `);
-        session.endDialog();
-         
+            session.send(`Gracias   ${session.dialogData.nombre}<br/> Saliendo del modo demo.. `);
+            session.endDialog();        
     }
 ])
 .triggerAction({
@@ -129,7 +108,7 @@ function getCardsAttachments(session) {
             .subtitle('Creamos aplicaciones web excepcionales.')
             .text('Valoramos las soluciones prácticas, diseños bellos e inteligentes, código limpio, buena arquitectura y, sobre todo, estamos comprometidos con el éxito de su empresa.')
             .images([
-                builder.CardImage.create(session, 'http://tekh-ne.com/Media/Default/Images/bots/desarrollo-movil.png')
+                builder.CardImage.create(session, 'http://tekh-ne.com/Media/Default/Images/bots/desarrollo-a-la-medida.png')
             ])
             .buttons([
                 builder.CardAction.openUrl(session, 'http://tekh-ne.com/servicios/web-application-development/index.html', 'Conozca más')
@@ -137,7 +116,7 @@ function getCardsAttachments(session) {
 
         new builder.HeroCard(session)
             .title('Desarrollo Móvil')
-            .subtitle('Process events with a serverless code architecture')
+            .subtitle('Creamos aplicaciones móviles geniales')
             .text('Proporcionamos servicios integrales para el desarrollo de aplicaciones móviles para todas las plataformas principales, incluyendo iOS de Apple y Android de Google')
             .images([
                 builder.CardImage.create(session, 'http://tekh-ne.com/Media/Default/Images/bots/desarrollo-movil.png')
@@ -151,7 +130,7 @@ function getCardsAttachments(session) {
             .subtitle('Desarrollamos aplicaciones de bases de datos para una variada clientela')
             .text('Nos centramos en aplicaciones de bases de datos personalizadas utilizando tecnologías avanzadas y personal experto para crear soluciones fáciles de usar, eficientes y confiables.')
             .images([
-                builder.CardImage.create(session, 'http://tekh-ne.com/Media/Default/Images/bots/desarrollo-movil.png')
+                builder.CardImage.create(session, 'http://tekh-ne.com/Media/Default/Images/bots/json-database-resources1.png')
             ])
             .buttons([
                 builder.CardAction.openUrl(session, 'http://tekh-ne.com/servicios/database-development/index.html', 'Conozca más')
